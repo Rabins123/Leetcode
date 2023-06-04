c43011b8572c1ab3894b4725166644de5e4b6a7d
Rabins Chaudhary <80695872+Rabins123@users.noreply.github.com>
2023-06-05 04:26:16 +0530

    Added solution - LeetHub

---
+++ Check If Circular Linked List - GFG/check-if-circular-linked-list.java
@@ -0,0 +1,102 @@
+//{ Driver Code Starts
+import java.util.*;
+
+class Node
+{
+	int data;
+	Node next;
+	Node(int d)
+	{
+		data = d;
+		next = null;
+	}
+}
+
+class Is_LinklIst_Circular
+{
+	Node head;
+	
+	
+	public static void main(String args[])
+	{
+	    Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		
+		while(t>0)
+		{
+			int n = sc.nextInt();
+			int l = sc.nextInt();
+			
+			sc.nextLine().trim().split(" ");
+			String str[]= sc.nextLine().trim().split(" ");
+            Node head = null, tail = null;
+            head = new Node(Integer.parseInt(str[0]));
+            tail = head;
+            for(int i = 1; i < n ; i++)
+            {
+                tail.next = new Node(Integer.parseInt( str[i]));
+                tail = tail.next;
+            }
+                
+			
+			if(l==1 && n>=1)
+			{
+				Node temp = head;
+				while(temp.next != null)
+				{
+					temp = temp.next; 
+				}
+				temp.next = head;
+			}
+			
+			GfG g = new GfG();
+			boolean b = g.isCircular(head);
+			if(b==true)
+			System.out.println(1);
+			else 
+			System.out.println(0);
+		t--;
+		}
+	}
+}
+
+
+// } Driver Code Ends
+
+
+/* Structure of LinkedList
+class Node
+{
+	int data;
+	Node next;
+	Node(int d)
+	{
+		data = d;
+		next = null;
+	}
+}
+*/
+class GfG
+{
+    boolean isCircular(Node head)
+    {
+	// Your code here
+	 if (head == null || head.next == null) {
+            return false;
+        }
+
+        Node current = head;
+
+        while (current != null) {
+            if (current.next == head) {
+                return true;  // Cycle found
+            }
+
+            Node temp = current.next;
+            current.next = head;  // Mark current node as visited
+            current = temp;
+        }
+
+        return false;  // No cycle found
+    }
+}
