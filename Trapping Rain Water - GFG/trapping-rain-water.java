90a005b06d4ba8c9d18f5831aad97fbce9e6c635
Rabins Chaudhary <80695872+Rabins123@users.noreply.github.com>
2023-06-24 16:08:00 +0530

    Added solution - LeetHub

---
+++ Trapping Rain Water - GFG/trapping-rain-water.java
@@ -0,0 +1,71 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+
+
+class Array {
+
+	public static void main (String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int t = Integer.parseInt(br.readLine().trim()); //Inputting the testcases
+		while(t-->0){
+		  
+		    //size of array
+		    int n = Integer.parseInt(br.readLine().trim());
+		    int arr[] = new int[n];
+		    String inputLine[] = br.readLine().trim().split(" ");
+		    
+		    //adding elements to the array
+		    for(int i=0; i<n; i++){
+		        arr[i] = Integer.parseInt(inputLine[i]);
+		    }
+		    
+		    Solution obj = new Solution();
+		    
+		    //calling trappingWater() function
+		    System.out.println(obj.trappingWater(arr, n));
+		}
+	}
+}
+
+
+// } Driver Code Ends
+
+
+class Solution{
+    
+    // arr: input array
+    // n: size of array
+    // Function to find the trapped water between the blocks.
+    static long trappingWater(int arr[], int n) { 
+        // Your code here
+         long totalWater = 0;
+         n = arr.length;
+
+        int[] leftMax = new int[n]; // Stores the maximum height encountered from the left side
+        int[] rightMax = new int[n]; // Stores the maximum height encountered from the right side
+
+        // maximum heightfrom the left  at each index
+        leftMax[0] = arr[0];
+        for (int i = 1; i < n; i++) {
+            leftMax[i] = Math.max(leftMax[i - 1], arr[i]);
+        }
+
+        //maximum height from the right at each index
+        rightMax[n - 1] = arr[n - 1];
+        for (int i = n - 2; i >= 0; i--) {
+            rightMax[i] = Math.max(rightMax[i + 1], arr[i]);
+        }
+
+        // Calculate the trapped water at each index
+        for (int i = 0; i < n; i++) {
+            int minMaxHeight = Math.min(leftMax[i], rightMax[i]);
+            totalWater += minMaxHeight - arr[i];
+        }
+
+        return totalWater;
+    } 
+}
+
+
